4cf882957f31cf06c7cab79403ae199568117f2a
sue herrera <sueherrera30@gmail.com>
2017-06-11 23:42:13 -0500

    imagenes

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,26 @@ $.getJSON("http://pokeapi.co/api/v2/pokemon", function(response){
 	constructorDePokemones(pokemons);
 });
 
-function constructorDePokemones(){
+
+function constructorDePokemones(pokemons){
+ var $pokeContenedor = $(".poke-contenedor");
 	
+pokemons.forEach(function(pokemon){
+	 /*creamos elementos*/
+	var $fila = $("<div/>", {"class":"row"});
+	var $contenedorPokemon = $("<div/>",{"class":"col s3 offset-s1 card"});
+	var $imagen = $("<img>",{"class":"responsive-img pokeImagen"});
+	$imagen.attr("src","assets/img/beedrill.png");
+	var $textoNombre = $("<p/>");
+	
+	$textoNombre.text(pokemons.name);
+	 $contenedorPokemon.data("nombre:"+pokemon.name);
+	
+	$contenedorPokemon.append($textoNombre);
+	$contenedorPokemon.append($imagen);
+	$contenedorPokemon.append($textoNombre);
+	$fila.append($contenedorPokemon);
+	$pokeContenedor.append($fila);
+
+ });
 }
